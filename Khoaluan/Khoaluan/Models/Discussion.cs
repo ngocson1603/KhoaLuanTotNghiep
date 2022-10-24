7b23381a4c0e35ace5e15c9ab53bc67446d2cac6
d09tri <hodangtri392001@gmail.com>
2022-10-24 10:20:54 +0700

    Thêm model Comment, Discussion

---
+++ Khoaluan/Khoaluan/Models/Discussion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Khoaluan.Models
+{
+    public class Discussion
+    {
+        public int GameId { get; set; }
+        public int Username { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public List<Comment> Comments { get; set; }
+    }
+}
