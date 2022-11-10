9f0a99c266e0fbc9589dd53091fcf48442986233
ngocson1603 <ngocson16032001@gmail.com>
2022-11-10 17:07:49 +0700

    refund v2

--- Khoaluan/Khoaluan/Interfaces/IRefundRepository.cs
+++ Khoaluan/Khoaluan/Interfaces/IRefundRepository.cs
@@ -5,5 +5,6 @@ namespace Khoaluan.Interfaces
     public interface IRefundRepository:IGameStoreRepository<Refund>
     {
         void refund(int userID, int productID, int OrderID);
+        int refundID(int userID, int productID);
     }
 }
