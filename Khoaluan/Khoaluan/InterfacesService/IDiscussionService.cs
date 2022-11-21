be3c85ade649b067147201c36c88d82111daea6f
vippro12356 <cowsepmy147@gmail.com>
2022-11-21 16:27:51 +0700

    add new refund

--- Khoaluan/Khoaluan/InterfacesService/IDiscussionService.cs
+++ Khoaluan/Khoaluan/InterfacesService/IDiscussionService.cs
@@ -1,7 +1,11 @@
-﻿namespace Khoaluan.InterfacesService
+﻿using Khoaluan.Models;
+using System.Collections.Generic;
+
+namespace Khoaluan.InterfacesService
 {
     public interface IDiscussionService
     {
         void comment(string postID, string userName, string message);
+        List<Discussion> listDiscussion(int id);
     }
 }
--- Khoaluan/Khoaluan/Models/Refund.cs
+++ Khoaluan/Khoaluan/Models/Refund.cs
@@ -1,10 +1,14 @@
-﻿namespace Khoaluan.Models
+﻿using System;
+
+namespace Khoaluan.Models
 {
     public class Refund:BaseEntity
     {
         public int UserID { get; set; }
         public int ProductID { get; set; }
+        public int Price { get; set; }
         public int Status { get; set; }
+        public DateTime DatePurchase { get; set; }
         public Users User { get; set; }
         public Product Product { get; set; }
     }
