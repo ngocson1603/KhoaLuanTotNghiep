618c649a6ce640586fe1bf93a7d71308f3c61d64
vippro12356 <cowsepmy147@gmail.com>
2022-12-12 22:43:52 +0700

    add xu ly create sale,check product is sale

---
+++ Khoaluan/Khoaluan/InterfacesRepository/ISaleProductRepository.cs
@@ -0,0 +1,10 @@
+﻿using Khoaluan.Models;
+using System.Collections.Generic;
+
+namespace Khoaluan.InterfacesRepository
+{
+    public interface ISaleProductRepository:IGameStoreRepository<SaleProduct>
+    {
+        List<int> currentSaleProduct();
+    }
+}
---
+++ Khoaluan/Khoaluan/OtpModels/SaleRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Khoaluan.OtpModels
+{
+    public class SaleRequest
+    {
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public List<SaleProductRequest> SaleProductRequests { get; set; }
+    }
+    public class SaleProductRequest
+    {
+        public int ProductID { get; set; }
+        public int Discount { get; set; }
+    }
+}
