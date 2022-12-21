b7a7109df04da20eb6d00356d62426af6fba621b
ngocson1603 <100844717+ngocson1603@users.noreply.github.com>
2022-12-21 10:45:08 +0700

    set template

--- Khoaluan/Khoaluan/ModelViews/SaleModelView.cs
+++ Khoaluan/Khoaluan/ModelViews/SaleModelView.cs
@@ -16,5 +16,8 @@ namespace Khoaluan.ModelViews
         public int Discount { get; set; }
         public int Status { get; set; }
         public decimal Price { get; set; }
+        public string NameSale { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
     }
 }
