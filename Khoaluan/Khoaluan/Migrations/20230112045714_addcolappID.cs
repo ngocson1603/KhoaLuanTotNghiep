7176a1fe2dd118f30d897a4d3eb4b36a0cff4bca
vippro12356 <cowsepmy147@gmail.com>
2023-01-12 11:59:13 +0700

    add col appid

---
+++ Khoaluan/Khoaluan/Migrations/20230112045714_addcolappID.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Khoaluan.Migrations
+{
+    public partial class addcolappID : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AppId",
+                table: "Product",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AppId",
+                table: "Product");
+        }
+    }
+}
